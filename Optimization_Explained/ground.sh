6c62f0f3f8731efc28e56c1451b90d370ff10341
PierpaoloSpadafora <pierpaolospadafora23+github@gmail.com>
2025-07-26 14:36:48 +0200

    fix

--- Optimization_Explained/ground.sh
+++ Optimization_Explained/ground.sh
@@ -2,6 +2,13 @@ clingo --mode=gringo 1.lp > out1
 clingo --mode=gringo 2.lp > out2
 clingo --mode=gringo 3.lp > out3
 
-grep -o 'scegli([^)]*)' out1 
-grep -o 'scegli([^)]*)' out2 
-grep -o 'scegli([^)]*)' out3 
+
+grep -o 'scegli([^)]*)' out1 | sed 's/scegli(//;s/)//' | sort -t',' -k1,1 -k2,2 -k3,3 -k4,4 | sed 's/^/scegli(/;s/$/)/' > out1
+
+grep -o 'scegli([^)]*)' out2 | sed 's/scegli(//;s/)//' | sort -t',' -k1,1 -k2,2 -k3,3 -k4,4 | sed 's/^/scegli(/;s/$/)/' > out2
+
+grep -o 'scegli([^)]*)' out3 | sed 's/scegli(//;s/)//' | sort -t',' -k1,1 -k2,2 -k3,3 -k4,4 | sed 's/^/scegli(/;s/$/)/' > out3
+
+diff out1 out2
+diff out1 out3
+diff out2 out3
